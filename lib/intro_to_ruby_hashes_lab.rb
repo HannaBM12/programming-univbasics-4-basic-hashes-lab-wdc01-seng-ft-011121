7530407434b6e9a9f065285696ae1ea6c6aefe8e
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-12-28 21:43:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -8,11 +8,15 @@ def my_hash
   new_hash = {
       :name => "James",
       :dept => "Marketing",
-      :id => 14265}
+      :id => 14265
+    }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    :name = "Grace Hopper"
+  }
 end
 
 
